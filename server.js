dea9db41e8eda1428112c89767d85c6f81fed3f4
BogieBogard <mbogard@utexas.edu>
2018-09-17 18:34:35 -0500

    removed 8080 as a port option

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var bodyParser = require("body-parser");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT
 
 // Sets up the Express app to handle data parsing
 
